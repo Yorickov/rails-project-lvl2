dbc28c0f3e5a25181262c9ae26228c9a14c3b183
yorickov <yorickov@gmail.com>
2021-08-23 18:48:49 +0300

    Add seed for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Category.destroy_all
+
+User.create!(
+  [
+    { email: 'admin@email.com',
+      password: 'adminsecret',
+      password_confirmation: 'adminsecret',
+      confirmed_at: Time.current },
+    { email: 'user@email.com',
+      password: 'usersecret',
+      password_confirmation: 'usersecret',
+      confirmed_at: Time.current }
+  ]
+)
+
+PostCategory.create!([{ name: 'sport' }, { name: 'science' }])
+
+Post.create!(
+  [
+    { user: User.first,
+      title: Faker::Lorem.sentence,
+      body: Faker::Lorem.paragraph_by_chars(number: 400),
+      post_category: PostCategory.first },
+    { user: User.second,
+      title: Faker::Lorem.sentence,
+      body: Faker::Lorem.paragraph_by_chars(number: 300),
+      post_category: PostCategory.second }
+  ]
+)
